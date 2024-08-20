292c3e7bebe86d88b2aaabbc664bc070c1302204
AliahX <40045439+AliahX@users.noreply.github.com>
2024-08-20 13:16:11 +1200

    add identifier to tracks and add functionality to get random songs from the music type

--- src/main/java/gay/aliahx/mixtape/MusicManager.java
+++ src/main/java/gay/aliahx/mixtape/MusicManager.java
@@ -1,14 +1,20 @@
 package gay.aliahx.mixtape;
 
+import com.google.common.base.CaseFormat;
 import com.google.common.collect.Maps;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
 
+import java.util.List;
 import java.util.Map;
+
+import static gay.aliahx.mixtape.Mixtape.client;
+import static gay.aliahx.mixtape.Mixtape.config;
 
 public class MusicManager {
     public Map<String, Entry> music;
@@ -29,15 +35,17 @@ public class MusicManager {
         }
     }
 
-
     public Entry getEntry(String name) {
         if(music.containsKey(name)) {
             return music.get(name);
         } else {
             JsonObject object = new JsonObject();
-            object.addProperty("name", (name.substring(0, 1).toUpperCase() + name.substring(1)).replaceAll("_", " "));
+            String nameP1 = name.substring(0, 1).toUpperCase();
+            String nameP2 = name.substring(1);
+            object.addProperty("name", (nameP1 + nameP2).replaceAll("_", " "));
             object.addProperty("artist", "");
             object.addProperty("album", "Unknown Album");
+            object.addProperty("identifier", "");
             return new Entry(object);
         }
     }
@@ -46,11 +54,21 @@ public class MusicManager {
         private final String name;
         private final String artist;
         private final String album;
+        private final Identifier identifier;
 
         public Entry(JsonObject json) {
             this.name = JsonHelper.getString(json, "name");
             this.artist = JsonHelper.getString(json, "artist");
             this.album = JsonHelper.getString(json, "album");
+            if(JsonHelper.hasElement(json, "identifier")) {
+                if (JsonHelper.getString(json, "identifier") == "") {
+//                    System.out.println("Missing identifier for " + name);
+                }
+                this.identifier = new Identifier(JsonHelper.getString(json, "identifier"));
+            } else {
+                this.identifier = new Identifier("");
+//                System.out.println("Missing identifier for " + name);
+            }
         }
 
         public String getName() {
@@ -61,6 +79,9 @@ public class MusicManager {
         }
         public String getAlbum() {
             return album;
+        }
+        public Identifier getIdentifier() {
+            return identifier;
         }
     }
 
@@ -84,5 +105,49 @@ public class MusicManager {
             return new AlbumCover(object.get("x").getAsInt(), object.get("y").getAsInt());
         }
         return new AlbumCover(0, 0);
+    }
+
+    public static SoundEvent getRandomSoundEvent(String musicType) {
+        String song;
+        List<String> songs = switch(musicType) {
+            case "minecraft:music.menu" -> config.menu.songs;
+            case "minecraft:music.creative" -> config.creative.songs;
+            case "minecraft:music.end" -> config.end.endSongs;
+            case "minecraft:music.dragon" -> config.end.dragonSongs;
+            case "minecraft:music.under_water" -> config.underwater.songs;
+            case "minecraft:music.credits" -> config.credits.songs;
+            case "minecraft:music.game" -> config.game.songs;
+            default -> {
+                if(musicType.contains("overworld")) {
+                    if(!config.game.specificOverworldMusic) {
+                        yield config.game.songs;
+                    }
+                    String zone = musicType.substring(26);
+                    String configListString = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, zone) + "Songs";
+                    try {
+                        yield (List<String>) config.game.getClass().getField(configListString).get(config.game);
+                    } catch (Exception ignored) {}
+                } else if(musicType.contains("nether")) {
+                    if(!config.nether.specificNetherMusic) {
+                        yield config.nether.songs;
+                    }
+                    String zone = musicType.substring(23);
+                    String configListString = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, zone) + "Songs";
+                    try {
+                        yield (List<String>) config.nether.getClass().getField(configListString).get(config.nether);
+                    } catch (Exception ignored) {}
+                }
+
+                Mixtape.LOGGER.error("Unknown music type: " + musicType);
+                yield config.game.songs;
+            }
+        };
+
+        if(songs.size() == 0) return SoundEvents.INTENTIONALLY_EMPTY;
+
+        song = songs.get((int) (Math.random() * songs.size()));
+        SoundEvent test = SoundEvent.of(Mixtape.musicManager.getEntry(song).getIdentifier());
+        System.out.println(test + " :3");
+        return test;
     }
 }
